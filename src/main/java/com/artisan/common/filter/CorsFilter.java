247e0cfcefaf91dc0e92f7113f080af8636d3d21
leeyom <635709492@qq.com>
2017-11-05 21:12:07 +0800

    新增一些注释，更新README.md文档

--- src/main/java/com/artisan/common/filter/CorsFilter.java
+++ src/main/java/com/artisan/common/filter/CorsFilter.java
@@ -45,19 +45,23 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         if (StringUtil.isNotEmpty(allowOrigin)) {
+            //允许访问的客户端域名，例如：http://web.xxx.com，若为*，则表示从任意域都能访问，即不做任何限制；
             response.setHeader("Access-Control-Allow-Origin", allowOrigin);
         }
-
         if (StringUtil.isNotEmpty(allowMethods)) {
+            //允许访问的请求方式，多个用逗号分割，例如：GET,POST,PUT,DELETE,OPTIONS；
             response.setHeader("Access-Control-Allow-Methods", allowMethods);
         }
         if (StringUtil.isNotEmpty(allowCredentials)) {
+            //是否允许请求带有验证信息，若要获取客户端域下的cookie时，需要将其设置为true；
             response.setHeader("Access-Control-Allow-Credentials", allowCredentials);
         }
         if (StringUtil.isNotEmpty(allowHeaders)) {
+            //允许服务端访问的客户端请求头，多个请求头用逗号分割，例如：Content-Type,Access-Token,timestamp
             response.setHeader("Access-Control-Allow-Headers", allowHeaders);
         }
         if (StringUtil.isNotEmpty(exposeHeaders)) {
+            //允许客户端访问的服务端响应头，多个响应头用逗号分割。
             response.setHeader("Access-Control-Expose-Headers", exposeHeaders);
         }
         chain.doFilter(req, res);
